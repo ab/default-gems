086706317c688125bb61336636ac29849aef39ae
Andy Brody <git@abrody.com>
2018-05-26 17:53:03 -0400

    Add a final rbenv rehash if available.

--- install.sh
+++ install.sh
@@ -4,3 +4,10 @@ set -eux
 ruby --version
 gem spec bundler >/dev/null || gem install bundler
 bundle check || bundle install
+
+if which rbenv >/dev/null; then
+    rbenv rehash
+fi
+
+set +x
+echo 'Finished'
